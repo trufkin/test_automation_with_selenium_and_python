fdad6522a8bcd2741dd2e005c2d42bc0fab77879
forspamuk <forspamuk@gmail.com>
2026-02-03 17:42:05 +0200

    added 2.4.7 implicit wait failure

---
+++ 2.4.7 Implicit wait failure.py
@@ -0,0 +1,19 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome()
+# говорим WebDriver ждать все элементы в течение 5 секунд
+browser.implicitly_wait(5)
+browser.get("http://suninjuly.github.io/wait2.html")
+
+try:
+    submit_button = browser.find_element(By.CSS_SELECTOR, "button")
+    button = browser.find_element(By.ID,"verify")
+    button.click()
+    message = browser.find_element(By.ID, "verify_message")
+    assert "successful" in message.text
+finally:
+    time.sleep(5)
+    browser.quit()
