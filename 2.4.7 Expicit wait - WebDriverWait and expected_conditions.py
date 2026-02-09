34d832c0b7a739538b6bcfda44b5478cd5766140
Alex Trufkin <alextrufkin@mymac-3.local>
2026-02-09 10:47:58 +0200

    Добавил 2.4.7 Expicit wait - WebDriverWait and expected_conditions.py

--- 2.4.7 Expicit wait - WebDriverWait and expected_conditions.py
+++ 2.4.7 Expicit wait - WebDriverWait and expected_conditions.py
@@ -2,4 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+import time
 
+browser = webdriver.Chrome()
+browser.get("http://suninjuly.github.io/wait2.html")
+
+try:
+    # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
+    button = WebDriverWait(browser,5).until(EC.element_to_be_clickable((By.ID, "verify")))
+    button.click()
+
+    message = browser.find_element(By.ID, "verify_message")
+    assert "success" in message.text
+finally:
+    time.sleep(5)
+    browser.quit()
